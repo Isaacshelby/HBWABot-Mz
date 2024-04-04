5bf6c1c9b92100010a11f4fa53277a923b6142a5
Isaacshelby <142240645+Isaacshelby@users.noreply.github.com>
2024-04-04 12:00:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.bot_language = 'lus' // I language code dah tur code hman i hriat loh chu
 
 //info
 global.owner = ['916909258087'] // I phone number in thlak rawh
-global.ownernumber = ['916909258087', '919366589620']// I phone number in thlak rawh
+global.ownernumber = '916909258087'// I phone number in thlak rawh
 global.ownername = "꧁Herbert♕꧂" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
 global.botname = "HBWABot Mizo" // I bot hming tur dah la
@@ -24,7 +24,7 @@ global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🤖'
 global.creator = "916909258087@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
-global.ownerNumber = ["916909258087@s.whatsapp.net","919366589620@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
+global.ownerNumber = "916909258087@s.whatsapp.net"// @ symbol hmaa number hi i phone number in thlak rawh
 global.prefa = ['','!','.','#','/']
 global.sessionName = 'session'
 
